646201fe2f73014759fa4b3bd314663227c9a61c
Alexis Amadei <alexisamadei@gmail.com>
2024-12-13 12:46:10 +0100

    comment unused code

--- server/routes/links/index.delete.ts
+++ server/routes/links/index.delete.ts
@@ -1,9 +1,11 @@
-import { links } from "../../database/schema/links";
-import { useDrizzle } from "../../utils/drizzle";
-import { eventHandler } from "h3";
+// import { links } from "../../database/schema/links";
+// import { useDrizzle } from "../../utils/drizzle";
+// import { eventHandler } from "h3";
 
-export default eventHandler(async (event) => {
-    const db = useDrizzle();
-    const delRes = await db.delete(links);
-    return delRes;
-})
+// export default eventHandler(async (event) => {
+//     const db = useDrizzle();
+//     const delRes = await db.delete(links);
+//     return {
+//         status
+//     }
+// })
