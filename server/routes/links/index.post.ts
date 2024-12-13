fe66a67e013a05f0997e06cd14ce1891d7548a08
Alexis Amadei <alexisamadei@gmail.com>
2024-12-13 12:46:00 +0100

    comment unused code

--- server/routes/links/index.post.ts
+++ server/routes/links/index.post.ts
@@ -2,17 +2,39 @@ import { defineEventHandler, readBody, createError } from "h3";
 import { links } from "../../database/schema";
 import { db } from "../../database";
 import generateSlug from "~/utils/generateSlug";
+// import { z } from "zod";
+// import { customAlphabet } from "nanoid";
+
+// const bodySchema = z.object({
+//     url: z.string().url(),
+//     title: z.string().optional(),
+//     slug: z.string().optional(),
+//     maxVisits: z.number().int().optional(),
+//     availableAt: z.string().optional(),
+//     expiresAt: z.string().optional(),
+//     createdAt: z.string().optional(),
+//     updatedAt: z.string().optional(),
+// })
 
 export default defineEventHandler(async (event) => {
+    // const nanoid = customAlphabet('1234567890abcdef', 5);
+    //     const body = await readValidatedBody(event, bodySchema.parse);
+    //     const slug = body.slug || nanoid();
+    //     const res = await db.insert(links).values({
+    //         slug: slug,
+    //         title: body.title
+    //     }).returning();
     try {
         const body = await readBody(event);
-
         if (body.url && body.title) {
-            const slug = generateSlug(body.title);
+            const slug = body.slug || generateSlug(body.title);
+            // fetch tags check differences
             const res = await db.insert(links).values({ slug, url: body.url, title: body.title }).returning();
+            setResponseStatus(event, 201);
             return {
-                status: 201,
-                body: res,
+                slug: slug,
+                url: body.url,
+                title: body.title,
             };
         } else {
             throw createError({
